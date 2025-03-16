5101052bf09b90de2c092e4bd0336190068ff008
daniil <dronzhin123@gmail.com>
2025-03-16 14:55:59 +0300

    update

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,18 @@
+package hexlet.code.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Url {
+    private Long id;
+    private String name;
+    private Timestamp createdAt;
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
