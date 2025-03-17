165f80f4f0bafcd9470b10098111c1fabd7f19ce
daniil <dronzhin123@gmail.com>
2025-03-17 16:28:54 +0300

    update

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -2,6 +2,6 @@ drop table if exists urls;
 
 create table urls (
     id bigint primary key auto_increment,
-    name varchar(255) not null,
-    createdAt timestamp not null
+    name varchar(255),
+    createdAt timestamp
 );
