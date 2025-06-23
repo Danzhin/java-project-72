c06bf037d3653e29df3194ac45fa812e6a9f9a42
five <dronzhin123@gmail.com>
2025-06-23 11:39:33 +0300

    update

--- app/src/test/java/hexlet/code/controller/UrlControllerTest.java
+++ app/src/test/java/hexlet/code/controller/UrlControllerTest.java
@@ -23,8 +23,6 @@ public class UrlControllerTest {
     @BeforeEach
     public final void setUp() throws SQLException {
         app = App.getApp();
-        UrlRepository.save("https://www.example.com");
-        UrlCheckRepository.save(1, 200, "h1", "title", "description");
     }
 
     @Test
@@ -40,6 +38,8 @@ public class UrlControllerTest {
     @Test
     public void readAllTest() {
         JavalinTest.test(app, (server, client) -> {
+            UrlRepository.save("https://www.example.com");
+            UrlCheckRepository.save(1, 200, "h1", "title", "description");
             var response = client.get(Routes.URLS_PATH);
             var responseBody = Objects.requireNonNull(response.body()).string();
             assertThat(response.code()).isEqualTo(200);
@@ -51,6 +51,8 @@ public class UrlControllerTest {
     @Test
     public void readByIdTest() {
         JavalinTest.test(app, (server, client) -> {
+            UrlRepository.save("https://www.example.com");
+            UrlCheckRepository.save(1, 200, "h1", "title", "description");
             var response = client.get(Routes.urlPath(1));
             var responseBody = Objects.requireNonNull(response.body()).string();
             assertThat(response.code()).isEqualTo(200);
@@ -65,10 +67,11 @@ public class UrlControllerTest {
     @Test
     public void createTest() {
         JavalinTest.test(app, (server, client) -> {
-            var requestBody = "name=https://www.example.com";
+            var requestBody = "url=https://www.example.comm";
             var response = client.post(Routes.URLS_PATH, requestBody);
+            var responseBody = Objects.requireNonNull(response.body()).string();
             assertThat(response.code()).isEqualTo(200);
-            assertThat(Objects.requireNonNull(response.body()).string()).contains("https://www.example.com");
+            assertThat(responseBody).contains("https://www.example.com");
         });
     }
 
